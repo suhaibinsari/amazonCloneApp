f2e6a9825f5b95686ecbdc4eb0bda039d9ea0763
suhaibinsari <mstar390@gmail.com>
2022-10-31 18:52:40 +0500

    inProgress

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Button from './components/buttons/Button'
+
+function App() {
+  return (
+    <div>
+      
+<Button/>
+
+<Button/>
+
+<Button/>
+
+<Button/>
+
+<Button/>
+
+<Button/>
+
+<Button/>
+
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/buttons/Button.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Button.css'
+function Button() {
+  return (
+    <div>
+        <button className='Btn'>Submit</button>
+    </div>
+  )
+}
+
+export default Button
